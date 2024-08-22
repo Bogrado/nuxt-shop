761733299dabd3b8aaf19682ae853ffe001cadd6
Bogrado <andreytrinuma@gmail.com>
2024-08-22 23:17:41 +0700

    expire fix

--- server/api/cart/anon_cart.post.ts
+++ server/api/cart/anon_cart.post.ts
@@ -3,6 +3,7 @@ import redis from '~/server/utils/redis'
 
 export default defineEventHandler(async event => {
   const config = useRuntimeConfig()
+  const expire = config.public.anonExpires / 1000
   const body = await readBody(event)
   const { sessionId, cartItems } = body
 
@@ -11,12 +12,7 @@ export default defineEventHandler(async event => {
   }
 
   // Сохраняем данные корзины в Redis с уникальным sessionId
-  await redis.set(
-    `cart:${sessionId}`,
-    JSON.stringify(cartItems),
-    'EX',
-    config.public.anonExpires
-  ) // Срок жизни - 1 час
+  await redis.set(`cart:${sessionId}`, JSON.stringify(cartItems), 'EX', expire) // Срок жизни - 1 час
 
   return { success: true }
 })
