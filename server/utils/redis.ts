75c9cb5305992fcfcb290cb2cf31db70b97dd4f7
Andrew <andreytrinuma@gmail.com>
2024-08-25 23:24:05 +0700

    remote redis settings

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -1,8 +1,9 @@
 import Redis from 'ioredis'
 
 const redis = new Redis({
-  host: '127.0.0.1', // Локальный адрес для Redis
+  host: '95.164.2.168', // IP адрес удаленного сервера, крутится в докере
   port: 6379, // Порт Redis (по умолчанию)
+  password: '111241698',
 })
 
 export default redis
